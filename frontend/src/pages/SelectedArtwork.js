bf21e586a2fb98ca8564d1ef0d8faf316bf2c1ae
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-06-22 10:19:18 +0200

    Added description in SelectedSrtwork to show description on profile page

--- frontend/src/pages/SelectedArtwork.js
+++ frontend/src/pages/SelectedArtwork.js
@@ -135,10 +135,7 @@ const SelectedArtworks = () => {
             <Text>Av {selectedArtwork.artist}, {selectedArtwork.year}</Text>
           </ArtistContainer>
           <Info>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
-            since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
+          {selectedArtwork.description}
           </Info>
           <TextClue><Span>Ledtråd:</Span> {selectedArtwork.clue}</TextClue>
           <form 
