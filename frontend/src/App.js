c0ded49ce5730a59c6689cd4bf3c5d3b7d884202
Åsa.Hildebrand <y>
2021-06-27 20:53:27 +0200

    Removed semicolon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 
 import MapContainer from './components/MapContainer'
 import SelectedArtwork from './pages/SelectedArtwork'
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Header from "./components/Header";
-import ProfilePage from "./pages/ProfilePage";
-import Cities from "./pages/Cities";
-import NavBar from "./components/NavBar";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Header from "./components/Header"
+import ProfilePage from "./pages/ProfilePage"
+import Cities from "./pages/Cities"
+import NavBar from "./components/NavBar"
 
-import user from "./reducers/user";
-import artwork from "./reducers/artwork";
-import city from "./reducers/city";
+import user from "./reducers/user"
+import artwork from "./reducers/artwork"
+import city from "./reducers/city"
 
 const reducer = combineReducers({
   user: user.reducer,
   artwork: artwork.reducer,
   city: city.reducer,
 });
-const store = configureStore({ reducer });
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
@@ -41,5 +41,5 @@ export const App = () => {
         </Provider>
       </BrowserRouter>
     </div>
-  );
-};
+  )
+}
